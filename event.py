1b0a8cbca989c4633edd8922ed1bd9f6705a439e
William Zhen <47043414+RandumbWilliam@users.noreply.github.com>
2022-10-07 18:49:51 -0400

    Event comments

--- event.py
+++ event.py
@@ -1,7 +1,7 @@
 class Event:
     def __init__(self, event_type, event_time):
-        self.event_type = event_type
-        self.event_time = event_time
+        self.event_type = event_type  # Event type of ARRIVAL, DEPART, or OBSERVATIONS
+        self.event_time = event_time  # Respective event time
 
     def __lt__(self, other):
-        return self.event_time < other.event_time
+        return self.event_time < other.event_time  # Less than operator for heappush
